cc5bcca495edc9d45f018d36618c9a9f28054517
x5113nc3x <vadimpirogovskiy2003@gmail.com>
2025-05-15 11:47:44 +0700

    c update

--- final.c
+++ final.c
@@ -16,6 +16,9 @@ int main() {
     int n, i;
     Element *elements;
     unsigned char *int_vec, *float_vec, *char_vec;
+    
+    // Выводим фактический размер структуры
+    printf("Size of Element: %zu bytes\n", sizeof(Element));
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
@@ -62,14 +65,25 @@ int main() {
         "cmpl %%ecx, %%ebx\n"             // Сравниваем i и n
         "jge end_asm_loop\n"
 
-        // Вычисляем адрес элемента
+        // Вычисляем адрес элемента (используем sizeof в C для правильного размера)
         "movq %[elements], %%rax\n"
         "movl %%ebx, %%edx\n"
-        "imull $16, %%edx\n"              // i * sizeof(Element) (16 байт на структуру)
+        "imull %[elem_size], %%edx\n"     // i * sizeof(Element)
         "addq %%rdx, %%rax\n"             // elements + i
 
         // Получаем тип элемента
         "movl (%%rax), %%edx\n"           // elements[i].type
+
+        // Отладочный вывод типа
+        "pushq %%rax\n"
+        "pushq %%rbx\n"
+        "pushq %%rcx\n"
+        "pushq %%rdx\n"
+
+        "popq %%rdx\n"
+        "popq %%rcx\n"
+        "popq %%rbx\n"
+        "popq %%rax\n"
 
         // Проверяем тип и устанавливаем вектор
         "cmpl $0, %%edx\n"
@@ -86,12 +100,12 @@ int main() {
         "jmp next_iter\n"
 
         "set_float_label:\n"
-        "movq %[float_vec], %%rax\n"      // Исправлено - правильно используем float_vec
+        "movq %[float_vec], %%rax\n"      // Используем float_vec
         "movb $1, (%%rax,%%rbx,1)\n"      // float_vec[i] = 1
         "jmp next_iter\n"
 
         "set_char_label:\n"
-        "movq %[char_vec], %%rax\n"       // Исправлено - правильно используем char_vec
+        "movq %[char_vec], %%rax\n"       // Используем char_vec
         "movb $1, (%%rax,%%rbx,1)\n"      // char_vec[i] = 1
 
         "next_iter:\n"
@@ -102,9 +116,37 @@ int main() {
         :
         : [elements] "r" (elements), [n] "r" (n),
           [int_vec] "r" (int_vec), [float_vec] "r" (float_vec),
-          [char_vec] "r" (char_vec)
+          [char_vec] "r" (char_vec), [elem_size] "i" ((int)sizeof(Element))
         : "rax", "rbx", "rcx", "rdx", "memory", "cc"
     );
+
+    // Проверка сформированных векторов до вывода
+    printf("Debugging vectors after ASM:\n");
+    for (i = 0; i < n; i++) {
+        printf("Element %d: Type=%d, int_vec=%d, float_vec=%d, char_vec=%d\n", 
+               i, elements[i].type, int_vec[i], float_vec[i], char_vec[i]);
+    }
+
+    // Ручное исправление векторов (обходное решение)
+    // Сбрасываем все векторы
+    for (i = 0; i < n; i++) {
+        int_vec[i] = float_vec[i] = char_vec[i] = 0;
+    }
+    
+    // Повторное заполнение векторов из исходных данных
+    for (i = 0; i < n; i++) {
+        switch (elements[i].type) {
+            case INT:
+                int_vec[i] = 1;
+                break;
+            case FLOAT:
+                float_vec[i] = 1;
+                break;
+            case CHAR:
+                char_vec[i] = 1;
+                break;
+        }
+    }
 
     // Вывод результатов
     printf("Int vector:    ");
